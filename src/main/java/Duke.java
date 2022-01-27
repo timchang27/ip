89318632f563a4cf6141a04de8c0fd74cff7769a
timchang27 <timothychang27@gmail.com>
2022-01-27 23:34:59 +0800

    Greet, Echo, Exit for Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,6 +12,14 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        String greet;
+        do {
+            greet = sc.nextLine();
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println(greet);
+            System.out.println("-------------------------------------------------------------------------");
+        }while(!greet.equals("bye"));
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
